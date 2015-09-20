b42b7e99b504e0f11e9dad0f4288aab940fcf2d0
sufanglei <fanglei.su@gmail.com>
2015-09-20 14:38:58 +0800

    add

---
+++ src/com/su/study/Bubble.java
@@ -0,0 +1,44 @@
+package com.su.study;
+
+/**
+ * Created by Administrator on 2015/9/19.
+ */
+public class Bubble {
+
+    public static void printResult(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            if (i < a.length - 1) {
+                System.out.print(a[i] + ",");
+            } else {
+                System.out.println(a[i]);
+            }
+        }
+    }
+
+    public static void sortArray(int[] a, String type) {
+        if (type.equals("min")) {
+            //每次比最少次数
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0, k = a.length - 1; j < k; j++, k--) {
+                    if (a[j] > a[j + 1]) {
+                        a[j] = a[j] + a[j + 1];
+                        a[j + 1] = a[j] - a[j + 1];
+                        a[j] = a[j] - a[j + 1];
+                    }
+                }
+            }
+        }
+    }
+    public static void sortArray(int[] a) {
+        //每次都比9次
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    a[j] = a[j] + a[j + 1];
+                    a[j + 1] = a[j] - a[j + 1];
+                    a[j] = a[j] - a[j + 1];
+                }
+            }
+        }
+    }
+}
