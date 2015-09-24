14b1a541415ba3b9ca320ab716df12851bc0e8f4
sufanglei <fanglei.su@gmail.com>
2015-09-24 22:16:07 +0800

    update

--- src/com/su/study/Main.java
+++ src/com/su/study/Main.java
@@ -10,6 +10,24 @@ public class Main {
         //Bubble.sortArray(a, "min");
         //Bubble.sortArray(a);
         //Bubble.printResult(a);
-
+        int a[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        for (int[] i : a){
+            for (int j : i){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        int b[][] = new int[3][3];
+        for (int i = 0;i<a.length;i++){
+            for (int j = 0;j<a[i].length;j++) {
+                b[j][i] = a [i][j];
+            }
+        }
+        for (int[] i : b){
+            for (int j : i){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
     }
 }
